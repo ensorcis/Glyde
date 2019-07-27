b90840a579591bfb54e558d7bf78c83b206d8bf0
DevAcharya-Dev <dev.acharya@ensorcis.com>
2019-07-27 15:46:39 -0400

    Login and SignUp UI completed Login: Page Checks for completed field SignUp: SignUp Button Does not validate fields and changes activity to main activity upon interacting with the button

--- app/src/main/java/com/example/glyde/Login.java
+++ app/src/main/java/com/example/glyde/Login.java
@@ -2,13 +2,35 @@ package com.example.glyde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void switchActivities(View v){
+        startActivity(new Intent(Login.this, signup.class));
+    }
+
+    public void chkInput(View view)
+    {
+        EditText email, pass;
+        email = findViewById(R.id.ET_Email);
+        pass = findViewById(R.id.ET_Password);
+
+        if (email.getText().toString().isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter Email",Toast.LENGTH_LONG).show();
+
+        if (pass.getText().toString().isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter Password",Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/glyde/signup.java
+++ app/src/main/java/com/example/glyde/signup.java
@@ -3,6 +3,9 @@ package com.example.glyde;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class signup extends AppCompatActivity {
 
@@ -11,5 +14,28 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+    }
+
+    public void vaildateInput(View view) {
+        EditText name, email, password;
+        name = findViewById(R.id.ET_Signup_Name);
+        email = findViewById(R.id.ET_SignUp_Email);
+        password = findViewById(R.id.ET_Signup_Pass);
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        setContentView(view);
+
+        if (email.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Enter email address", Toast.LENGTH_SHORT).show();
+        } else {
+            if (email.getText().toString().trim().matches(emailPattern)) {
+                Toast.makeText(getApplicationContext(), "Valid email address", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Invalid email address", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (name.getText().toString().isEmpty() || password.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Fields Empty", Toast.LENGTH_LONG).show();
+        }
     }
 }
